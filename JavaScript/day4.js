4d8eddec1fa3af94343289f7088a50d502dd0458
unknown <gandhirajbalu@gmail.com>
2025-08-06 00:35:20 +0530

    Adding day4 js

---
+++ JavaScript/day4.js
@@ -0,0 +1,76 @@
+//if else
+    if(5>4){
+        console.log("True");
+    }else {
+        console.log("False");
+    }
+    if(10<5){
+        console.log("Condition is True");
+    }else {
+        console.log("Condition is False");
+    }
+    if(null){
+        console.log("Hello");
+    }else {
+        console.log("Hello1");
+    }
+    if(11>10){
+        console.log("Condition is True");
+        console.log("Condition is True1");
+    }else {
+        console.log("Condition is False");
+    }
+    if(" "){
+        console.log("Condition is True");
+        console.log("Condition is True1");
+    }else {
+        console.log("Condition is False");
+    }
+    //Ladder
+    let mark = 90;
+    if(mark>=95){
+        console.log("A+");
+    }else if (mark>=90) {
+            console.log("A");
+    }else if(mark>=85){
+        console.log("B");
+    }else if (mark>=80){
+        console.log("C");
+    }
+    let mark1 = 90;
+    if(mark1>=95){
+        console.log("A+");
+    } if (mark1>=90) {
+            console.log("A");
+    } if(mark1>=85){
+        console.log("B");
+    } if (mark1>=80){
+        console.log("C");
+    }
+    //Iterative
+    //For
+    for (let i=0; i<10; i+=2) {
+        console.log("iteration: " + i);
+    }
+    let arr = [1,2,3,4,5,6];
+    for(let i=0; i<6; i+=2){
+    console.log("iteration: " + arr[i]);
+    }
+    //While
+    let i = 0;
+    while (i<arr.length){
+        console.log("Array element: " + arr[i]);
+        i++;
+    }
+    //do while
+    // let i = 0;
+    // do {
+    //     console.log("do while array element: " + arr[i]);
+    //     i++;
+    // }while (i<arr.length);
+
+    
+
+     
+
+
